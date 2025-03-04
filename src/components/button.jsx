3e3f049890afffbd96401f0217c10b6086dff171
shahbaz12243 <shahbazz12243@gmail.com>
2025-03-04 14:49:19 +0530

    done

---
+++ src/components/button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ViewProductButton = () => {
+    return (
+        <button>View Product</button>
+    );
+};
+
+export default ViewProductButton;
---
+++ src/components/productcard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ViewProductButton from './button';
+
+const ProductCard = () => {
+    const productImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoUALWG-2fpbSUPbiVSUNHjPbi2SzaPQA21g&s';
+    const productName = 'Headphones';
+    const productPrice = '$99.99';
+
+    return (
+        <div className="product-card">
+            <img src={productImage} alt={productName} />
+            <h2>{productName}</h2>
+            <p>{productPrice}</p>
+            <ViewProductButton />
+        </div>
+    );
+};
+
+export default ProductCard;
